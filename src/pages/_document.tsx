cfd106e7b4909418a3954b489ff3785a23a6746e
robsonhr16 <robhenrod@gmail.com>
2021-08-23 19:20:32 -0300

    Fix for SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,10 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta name="description" content="Small system created as a test for a FrontEnd Developer position at SAS Educação" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
